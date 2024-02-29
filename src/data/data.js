7db77d3f837e70fa3fcda5892cf7f4853f7eb0b2
toharimaolana <sitohari01@gmail.com>
2024-02-29 20:29:03 +0700

    Commit data

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        id: 0,
+        question: 'Apakah seorang pemula bisa ikut belajar?',
+        answer: 'SiJago menyediakan kelas gratis Web Developement dan UI/UX Design, Kalin bisa mengaksesnya kelas-kelas yang tersedia. '
+    },
+    {
+        id: 1,
+        question: 'Apakah course sijago berbayar ',
+        answer: 'Tentu saja course SiJago gratis, tanpa dipungut biaya dan kalian semua bisa akses semua kelas.'
+    },
+    {
+        id: 2,
+        question: 'Bagaimana cara mendaftar dalam kursus online',
+        answer: 'Anda perlu membuat akun pada SiJago. Setelah itu, pilih kursus yang diinginkan, dan ikuti petunjuk pendaftarannya.'
+    }
+  ]
